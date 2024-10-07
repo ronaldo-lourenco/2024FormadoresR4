cc4b6871dff7e29276d65c2a04488ba435962306
ronaldo-lourenco <ronaldo.lourenco@escola.pr.gov.br>
2024-10-07 18:04:05 +0000

    Atualização Index e JS.

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,8 +1,9 @@
 const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
-    const res = await fetch(url)
-    const dados = await res.json()
+    const resposta = await fetch(url)
+    const dados = await resposta.json()
+    
     const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parseInt(dados.tempo_medio)
